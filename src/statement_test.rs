110c3ea815432a382715b6e34e4c17e1991e3de2
SiesslPhillip <phillip.siessl@grueneerde.com>
2026-02-11 11:47:12 +0100

    [feature] adjust tests

--- src/statement_test.rs
+++ src/statement_test.rs
@@ -1,10 +1,16 @@
 #[cfg(test)]
 mod tests {
+    use std::fs;
+    use crate::cursor::Cursor;
     use crate::statement::{InsertError, insert, select};
+    use crate::table::Table;
 
     #[test]
     fn insert_returns_error_if_not_enough_args() {
-        let res = insert("insert 1 alice");
+        let test_database_name = String::from("test_db");
+        let mut table = Table::db_open(&test_database_name).unwrap();
+        let curr = &mut Cursor::new(&mut table);
+        let res = insert(curr,"insert 1 alice");
         assert!(matches!(
             res,
             Err(InsertError::NotEnoughArgs {
@@ -16,9 +22,19 @@ mod tests {
 
     #[test]
     fn insert_returns_id_on_success() {
-        let res = insert("insert 42 alice alice@example.com");
+        let test_database_name = String::from("test_db");
+        let mut table = Table::db_open(&test_database_name).unwrap();
+        let mut cur = Cursor::new(&mut table);
+
+        let res = insert(&mut cur, "insert 42 alice alice");
+        cur.table.db_close().unwrap();
+
         assert_eq!(res, Ok(42));
-        let output = select("select 42").unwrap();
+
+        let output = select(&mut cur, "select 42").unwrap();
         assert_eq!(output, 42i32);
+
+        drop(cur);
+        fs::remove_file("test_db").unwrap();
     }
 }
